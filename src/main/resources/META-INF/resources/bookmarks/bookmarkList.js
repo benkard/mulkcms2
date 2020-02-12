22f623c810297467da2d8662c5d4b90ae8e84dae
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2020-02-12 05:54:53 +0100

    Bookmark submission: Automatically fetch title after URI input.
    Change-Id: Ieea258e076d1dbaeba2520e583b590822dfdcab8

--- src/main/resources/META-INF/resources/bookmarks/bookmarkList.js
+++ src/main/resources/META-INF/resources/bookmarks/bookmarkList.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   let bookmarkSubmissionPane = document.getElementById('bookmark-submission-pane');
+  let titleInput = document.getElementById('title-input');
   let uriInput = document.getElementById('uri-input');
+  let uriSpinner = document.getElementById('uri-spinner');
+
   bookmarkSubmissionPane.addEventListener('opened', () => uriInput.focus());
+
+  uriInput.addEventListener('blur', async () => {
+    uriSpinner.hidden = false;
+    uriSpinner.playing = true;
+    let r = await fetch(uriInput.value);
+    uriSpinner.hidden = true;
+    uriSpinner.playing = false;
+
+    if (!r.ok) {
+      return;
+    }
+
+    let html = await r.text();
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, "text/html");
+    let titles = doc.getElementsByTagName('title');
+    if (titles.length <= 0) {
+      return;
+    }
+    titleInput.value = titles[0].innerText;
+  });
 });
+
