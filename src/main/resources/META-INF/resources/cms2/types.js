e5f071f19a0175d72837cce72acc9cb8fe398db9
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2020-02-15 21:33:55 +0100

    Web: Add ESLint.
    Change-Id: I3c7fb3cb2b11de29dab0f3be0d1587ee3d41e72f

--- src/main/resources/META-INF/resources/cms2/types.js
+++ src/main/resources/META-INF/resources/cms2/types.js
@@ -1,8 +1,9 @@
 // @flow
 
+/** Casts an object to an arbitrary type. */
 export function cast/*:: <T>*/(x /*: ?Object*/) /*: T*/ {
-  if (x === null || x === undefined) {
-    throw "unexpected null or undefined";
+  if (x === null || typeof x === 'undefined') {
+    throw new Error("unexpected null or undefined");
   } else {
     /*:: (x: T); */
     return x;
