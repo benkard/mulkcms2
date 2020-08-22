6cc0c7a8be47bcde8716297e72c7f78979157ae9
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2020-08-22 20:13:28 +0200

    Scala: Fewer semicolons.
    Change-Id: I1ca6f4b4bcd732602f16c7114d7bb91c5a5fcd92

--- src/main/scala/eu/mulk/mulkcms2/cms/about/AboutResource.scala
+++ src/main/scala/eu/mulk/mulkcms2/cms/about/AboutResource.scala
@@ -1,12 +1,12 @@
-package eu.mulk.mulkcms2.cms.about;
+package eu.mulk.mulkcms2.cms.about
 
-import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
-import io.quarkus.qute.api.ResourcePath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import io.quarkus.qute.Template
+import io.quarkus.qute.TemplateInstance
+import io.quarkus.qute.api.ResourcePath
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
 
 @Path("/about")
 class AboutResource {
