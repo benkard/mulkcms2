709e226d2b4cf6935c37cebb64172731d97f5cb2
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2020-08-23 11:09:01 +0200

    Scala: More compact imports.
    Change-Id: I54a8ff7825ce443ad82ce34ae4c6c1a331382a6e

--- src/main/scala/eu/mulk/mulkcms2/cms/about/AboutResource.scala
+++ src/main/scala/eu/mulk/mulkcms2/cms/about/AboutResource.scala
@@ -1,11 +1,8 @@
 package eu.mulk.mulkcms2.cms.about
 
-import io.quarkus.qute.Template
-import io.quarkus.qute.TemplateInstance
+import io.quarkus.qute.{Template, TemplateInstance}
 import io.quarkus.qute.api.ResourcePath
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.{GET, Path, Produces}
 import javax.ws.rs.core.MediaType
 
 @Path("/about")
