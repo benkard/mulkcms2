64bf1091258477f505bf07939007440f86d33254
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2022-05-02 20:29:56 +0200

    MarkdownConverter: Enable FootnoteExtension.
    Change-Id: I6c7018edfd4e41e54390c397a3ba675d30ccabc6

--- src/main/java/eu/mulk/mulkcms2/common/markdown/MarkdownConverter.java
+++ src/main/java/eu/mulk/mulkcms2/common/markdown/MarkdownConverter.java
@@ -3,6 +3,7 @@ package eu.mulk.mulkcms2.common.markdown;
 import com.vladsch.flexmark.ext.abbreviation.AbbreviationExtension;
 import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
 import com.vladsch.flexmark.ext.definition.DefinitionExtension;
+import com.vladsch.flexmark.ext.footnotes.FootnoteExtension;
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.ext.typographic.TypographicExtension;
@@ -30,12 +31,14 @@ public class MarkdownConverter {
             AbbreviationExtension.create(),
             AutolinkExtension.create(),
             DefinitionExtension.create(),
-            TypographicExtension.create()));
+            TypographicExtension.create(),
+            FootnoteExtension.create()));
     options.set(Parser.FENCED_CODE_BLOCK_PARSER, true);
     options.set(HtmlRenderer.SOFT_BREAK, "\n");
     options.set(HtmlRenderer.HARD_BREAK, "<br />\n");
     options.set(TypographicExtension.ENABLE_SMARTS, true);
     options.set(TypographicExtension.ENABLE_QUOTES, true);
+    options.set(FootnoteExtension.FOOTNOTE_BACK_REF_STRING, "");
 
     this.parser = Parser.builder(options).build();
     this.renderer = HtmlRenderer.builder(options).build();
