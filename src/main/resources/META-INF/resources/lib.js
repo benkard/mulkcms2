2137b35a465ae9f830519c5a8bd49cb388fd552d
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2022-11-01 10:38:20 +0100

    Wiki: Rebundle Content Tools.
    Content Tools was previously wrapped into an ECMAScript module through
    the use of Snowpack.  This mechanism broke some time ago, making the
    wiki uneditable.
    
    Content Tools is now imported as is, which simplifies its packaging
    and restores it to a working state.
    
    Change-Id: I49d2d625bc955a260a63c47bcaceb23b61366123

--- src/main/resources/META-INF/resources/lib.js
+++ src/main/resources/META-INF/resources/lib.js
@@ -4,6 +4,7 @@ import './lazychat/MlkLazychatSubmissionForm.js';
 import './lazychat/newLazychatMessage.js';
 import './posts/commentBox.js';
 import './posts/postList.js';
+import './wiki/wikiPage.js';
 
 import './web_modules/elix/define/Button.js';
 import './web_modules/elix/define/ExpandablePanel.js';
---
+++ src/main/resources/META-INF/resources/wiki/wikiPage.js
@@ -0,0 +1,53 @@
+import '../web_modules/ContentTools/build/content-tools.min.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  let editor = ContentTools.EditorApp.get();
+  editor.init('*[data-editable]', 'data-name');
+
+  editor.addEventListener('saved', async function (ev) {
+    document.getElementById("warning-panel").close();
+
+    let {regions} = ev.detail();
+    if (Object.getOwnPropertyNames(regions).length === 0) {
+      // Nothing changed.
+      return;
+    }
+
+    this.busy(true);
+
+    let requestParams = new URLSearchParams();
+    for (let name of Object.getOwnPropertyNames(regions)) {
+      requestParams.append(name, regions[name]);
+    }
+
+    let {pageTitle} = document.getElementById('wiki-page').dataset;
+
+    let response = await fetch(`/wiki/${pageTitle}`, {
+      method: 'POST',
+      body: requestParams
+    });
+
+    if (!response.ok) {
+      document.getElementById("warning-panel").open();
+      document.getElementById("warning-text").innerText = `Failed to save page: ${response.statusText}`;
+      this.busy(false);
+
+return;
+    }
+
+    let status = await response.json();
+    if (status.status !== "ok") {
+      document.getElementById("warning-panel").open();
+      document.getElementById("warning-text").innerText = `Failed to save page: ${JSON.stringify(status)}`;
+      this.busy(false);
+
+return;
+    }
+
+    if (status.hasOwnProperty("content")) {
+      document.getElementById("wiki-content").innerHTML = status.content;
+    }
+
+    this.busy(false);
+  });
+});
